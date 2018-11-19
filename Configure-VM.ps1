abae7a921b39ee9cfda6af77d016caee3edc0583
wkasdorp <wkasdorp@hotmail.com>
2018-11-19 14:17:56 +0100

    added explicit exit value to please extension

--- Configure-VM.ps1
+++ Configure-VM.ps1
@@ -7,11 +7,17 @@ Write-Output "Written from Configure-VM" > $logfile
 try {
     Enable-PSRemoting -force -ErrorAction Stop
     Write-Output "Enabled PSremoting" >> $logfile
+
+    # explicit error code need for custom script extension
+    exit 0
 } catch {
     Write-Output "Enabling PSremoting failed ($_.Exception.Message)" >> $logfile
+
+    # explicit error code need for custom script extension
+    exit 1
 }
 
-
+exit 0
 #
 # use this to generate Base64 for inline scripts
 # [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes('Enable-PSRemoting -force')) | Set-Clipboard
